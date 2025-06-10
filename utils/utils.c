5a76b73ecad572ab12458d63f3bb919d9f95bba7
Gabriel Nathan <devgabrielnathan@gmail.com>
2025-06-10 13:07:06 -0300

    chore: re-added menu options

--- utils/utils.c
+++ utils/utils.c
@@ -15,14 +15,13 @@ void color_printf(char *text, ColorText color)
 void menu() {
     color_printf("\n----- Loja de Roupas -----\n", COLOR_GREEN);
     color_printf("1. Registrar venda.\n", COLOR_RESET);
-    color_printf("2. Sair.\n", COLOR_RED);
+    color_printf("2. Listar vendas do dia.\n", COLOR_RESET);
+    color_printf("3. Consultar faturamento bruto.\n", COLOR_RESET);
+    color_printf("4. Consultar quantidade de clientes que realizaram compras.\n", COLOR_RESET);
+    color_printf("5. Consultar item mais vendido.\n", COLOR_RESET);
+    color_printf("6. Consultar item menos vendido.\n", COLOR_RESET);
+    color_printf("7. Sair.\n", COLOR_RED);
     color_printf("Escolha uma opção: ", COLOR_WHITE);
-    // color_printf("2. Listar vendas do dia.\n", COLOR_RESET);
-    // color_printf("3. Consultar faturamento bruto.\n", COLOR_RESET);
-    // color_printf("4. Consultar quantidade de clientes.\n", COLOR_RESET);
-    // color_printf("5. Consultar item mais vendido.\n", COLOR_RESET);
-    // color_printf("6. Consultar item menos vendido.\n", COLOR_RESET);
-    // color_printf("7. Sair.\n", COLOR_RED);
 }
 
 void limparTerminal() {
