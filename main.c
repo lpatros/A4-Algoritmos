bd7cd73f553993048c54705fdbf0c081d450a635
Gabriel Nathan <devgabrielnathan@gmail.com>
2025-06-14 20:38:10 -0300

    update

--- main.c
+++ main.c
@@ -33,60 +33,61 @@ void main() {
 
         clearTerminal();
 
+        if (option < 1 || option > 7) {
+            color_printf("Opcao invalida.\n", COLOR_RED);
+            continue;
+        }
+
+        if (option == 1) {
+            newSale.id = saleList.nextClientId;
+
+            printf("--- Registrar Nova Venda ---\n");
+            printf("Digite o código do item: ");
+            scanf("%d", &newSale.codeItem);
+            clearInputBuffer();
+
+            printf("Digite o nome do item: ");
+            fgets(newSale.nameItem, sizeof(newSale.nameItem), stdin);
+            newSale.nameItem[strcspn(newSale.nameItem, "\n")] = 0;
+
+            printf("Digite a marca do item: ");
+            fgets(newSale.markItem, sizeof(newSale.markItem), stdin);
+            newSale.markItem[strcspn(newSale.markItem, "\n")] = 0;
+
+            printf("Digite a quantidade: ");
+            scanf("%d", &newSale.quantity);
+            clearInputBuffer();
+
+            printf("Digite o preço unitário: ");
+            scanf("%f", &newSale.unitPrice);
+            clearInputBuffer();
+
+            saveSale(&saleList, newSale);
+            summaryPurchase(saleList);
+        }
+
+        if (option == 7) {
+            color_printf("Saindo...\n", COLOR_YELLOW);
+            return;
+        }
+
+        consultationDate = getConsultationDate();
+
         switch (option) {
-            case 1:
-                newSale.id = saleList.nextClientId;
-
-                printf("--- Registrar Nova Venda ---\n");
-                printf("Digite o código do item: ");
-                scanf("%d", &newSale.codeItem);
-                clearInputBuffer();
-
-                printf("Digite o nome do item: ");
-                fgets(newSale.nameItem, sizeof(newSale.nameItem), stdin);
-                newSale.nameItem[strcspn(newSale.nameItem, "\n")] = 0;
-
-                printf("Digite a marca do item: ");
-                fgets(newSale.markItem, sizeof(newSale.markItem), stdin);
-                newSale.markItem[strcspn(newSale.markItem, "\n")] = 0;
-
-                printf("Digite a quantidade: ");
-                scanf("%d", &newSale.quantity);
-                clearInputBuffer();
-
-                printf("Digite o preço unitário: ");
-                scanf("%f", &newSale.unitPrice);
-                clearInputBuffer();
-
-                saveSale(&saleList, newSale);
-                summaryPurchase(saleList);
-
-                break;
             case 2:
-                consultationDate = getConsultationDate();
                 listSaleByDay(saleList, consultationDate);
                 break;
             case 3:
-                consultationDate = getConsultationDate();
                 displayBillingPerDay(saleList, consultationDate);
                 break;
             case 4:
-                consultationDate = getConsultationDate();
                 displayNumberOfCustomersPerDay(saleList, consultationDate);
                 break;
             case 5:
-                consultationDate = getConsultationDate();
                 displayBestsellingProductPerDay(saleList, consultationDate);
                 break;
             case 6:
-                consultationDate = getConsultationDate();
                 displayProductLeastSoldPerDay(saleList, consultationDate);
-                break;
-            case 7:
-                isRunning = 0;
-                break;
-            default:
-                color_printf("Opcao nao implementada ainda.\n", COLOR_YELLOW);
                 break;
         }
     }
